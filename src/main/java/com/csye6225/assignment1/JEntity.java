20f358bca4f5132ed7de9b3fe3864859e38dd530
hetashavi <shah.heta@northeastern.edu>
2019-01-30 18:22:17 -0500

    JEntity class

---
+++ src/main/java/com/csye6225/assignment1/JEntity.java
@@ -0,0 +1,18 @@
+package com.csye6225.assignment1;
+
+public class JEntity {
+    // private int code;
+    private String msg;
+
+
+
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
+
