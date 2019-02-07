c48a95bcd199ec1474e8e23ce859999f888a37b4
chaitanyaj2019 <joshi.chai@northeastern.edu>
2019-02-07 14:29:57 -0500

    validate template logic added in aws cloud formation

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -6,6 +6,13 @@ then
 	exit 1
 fi
 stack_name=$1
+aws cloudformation validate-template --template-body file://csye6225-cf-networking.json >/dev/null 2>&1
+ret=$?
+if [ $ret -ne 0 ];
+then
+        echo "Error in template"
+        exit $ret
+fi
 aws cloudformation create-stack --stack-name $stack_name --template-body file://csye6225-cf-networking.json
 ret=$?
 if [ $ret -eq 0 ];
