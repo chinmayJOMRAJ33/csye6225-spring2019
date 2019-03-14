488691f811c01a56b98ec319d265049f9bf78597
poojaalone <alone.p@husky.neu.edu>
2019-03-14 01:19:07 -0400

    Application stack AMI selection

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 NET_STACK_NAME=$1
-keyPair=$2
+centoskey=$2
 EC2="${NET_STACK_NAME}-csye6225-ec2"
 
 export vpcID=$(aws ec2 describe-vpcs --filters "Name=tag-key,Values=Name" --query "Vpcs[*].[CidrBlock, VpcId][-1]" --output text|grep 10.0.0.0/16|awk '{print $2}')
@@ -11,9 +11,14 @@ export subnet2=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpcID" 
 
 export subnet3=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpcID" --query 'Subnets[*].[SubnetId, VpcId, AvailabilityZone, CidrBlock]' --output text|grep 10.0.14.0/24|grep us-east-1c|awk '{print $1}')
 
-export AMI=$(aws ec2 describe-images --filters "Name=name,Values=csye6225" --query 'sort_by(Images, &CreationDate)[-1].ImageId' --output text)
+export AMI=$(aws ec2 describe-images --filters "Name=name,Values=csye6225*" "Name=root-device-type,Values=ebs" --query 'sort_by(Images, &CreationDate)[-1].ImageId' --output text)
 
-aws cloudformation create-stack --stack-name $NET_STACK_NAME --template-body file://csye6225-cf-application.json --parameters ParameterKey=VpcId,ParameterValue=$vpcID ParameterKey=EC2Name,ParameterValue=$EC2 ParameterKey=SubnetId1,ParameterValue=$subnet1 ParameterKey=SubnetId2,ParameterValue=$subnet2 ParameterKey=SubnetId3,ParameterValue=$subnet3 ParameterKey=AMI,ParameterValue=$AMI ParameterKey=keyName,ParameterValue=$keyPair
+export  keyPair=$(aws ec2 describe-key-pairs --key-name $centoskey)
+echo $keyPair
+
+echo "ami id is : $AMI"
+
+aws cloudformation create-stack --stack-name $NET_STACK_NAME --template-body file://csye6225-cf-application.json --parameters ParameterKey=VpcId,ParameterValue=$vpcID ParameterKey=EC2Name,ParameterValue=$EC2 ParameterKey=SubnetId1,ParameterValue=$subnet1 ParameterKey=SubnetId2,ParameterValue=$subnet2 ParameterKey=SubnetId3,ParameterValue=$subnet3 ParameterKey=AMI,ParameterValue=$AMI ParameterKey=keyName,ParameterValue=$centoskey
 
 
 export STACK_STATUS=$(aws cloudformation describe-stacks --stack-name $NET_STACK_NAME --query "Stacks[][ [StackStatus ] ][]" --output text)
