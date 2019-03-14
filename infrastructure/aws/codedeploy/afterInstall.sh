efe27284c36bde0f73db7972bc95a603c2f41cd2
poojaalone <alone.p@husky.neu.edu>
2019-03-14 16:38:22 -0400

    appspecfile changes and afterinstall changes

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -4,11 +4,8 @@ sudo systemctl stop tomcat.service
 
 sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/assignment1-0.0.1-SNAPSHOT
 
-sudo chown tomcat:tomcat /opt/tomcat/webapps/assignment1-0.0.1-SNAPSHOT.jar
+sudo chown tomcat:tomcat /opt/tomcat/webapps/assignment1-0.0.1-SNAPSHOT.war
 
-java -jar assignment1-0.0.1-SNAPSHOT.jar
-
-# cleanup log files
 sudo rm -rf /opt/tomcat/logs/catalina*
 sudo rm -rf /opt/tomcat/logs/*.log
 sudo rm -rf /opt/tomcat/logs/*.txt
