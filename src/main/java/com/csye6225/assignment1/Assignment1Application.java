28ab29d0b435a41ab18a26af881c76d3ea30b07c
chaitanyaj2019 <chaitanyajoshi@localhost.localdomain>
2019-03-16 10:10:01 -0400

    adding application builder setting

--- src/main/java/com/csye6225/assignment1/Assignment1Application.java
+++ src/main/java/com/csye6225/assignment1/Assignment1Application.java
@@ -6,19 +6,19 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
-//extends SpringBootServletInitializer
+//
 @SpringBootApplication
-public class Assignment1Application {
+public class Assignment1Application extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(Assignment1Application.class, args);
     }
 
 
-   // @Override
-   // protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
-//
-  //      return application.sources(Assignment1Application.class);
-   // }
+    @Override
+    protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
+
+        return application.sources(Assignment1Application.class);
+   }
 }
 
