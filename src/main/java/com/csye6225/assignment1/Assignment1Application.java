64f480e3d7edb652fa8f3801ed23b2e10d7a325a
chaitanyaj2019 <chaitanyajoshi@localhost.localdomain>
2019-03-16 10:00:33 -0400

    removing additional application spec

--- src/main/java/com/csye6225/assignment1/Assignment1Application.java
+++ src/main/java/com/csye6225/assignment1/Assignment1Application.java
@@ -5,18 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
+//extends SpringBootServletInitializer
 @SpringBootApplication
-public class Assignment1Application extends SpringBootServletInitializer {
+public class Assignment1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Assignment1Application.class, args);
     }
 
 
-    @Override
-    protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
-
-        return application.sources(Assignment1Application.class);
-    }
+   // @Override
+   // protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
+//
+  //      return application.sources(Assignment1Application.class);
+   // }
 }
 
