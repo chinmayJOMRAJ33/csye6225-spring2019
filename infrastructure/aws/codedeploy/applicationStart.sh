c8134990ac5bb27c67f768e21574f5c69c4fca64
chaitanyaj2019 <chaitanyajoshi@localhost.localdomain>
2019-03-16 16:09:49 -0400

    resolved permission issues

--- infrastructure/aws/codedeploy/applicationStart.sh
+++ infrastructure/aws/codedeploy/applicationStart.sh
@@ -3,5 +3,5 @@
 #cd /opt/tomcat/webapps
 #java -jar assignment1-0.0.1-SNAPSHOT.war
 sudo systemctl start tomcat.service
-sudo chmod 777 assignment1-0.0.1-SNAPSHOT
+sudo chmod 777 /opt/tomcat/webapps/assignment1-0.0.1-SNAPSHOT
 
